0d1fde1417f97cb219b7c9090dbfa8878a03e984
timvk <tim_vk@abv.bg>
2015-10-14 12:26:24 +0300

    + base controller

---
+++ Champ.App/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Champ.App.Controllers
+{
+    using System.Web.Mvc;
+    using Data;
+    using Data.UnitOfWork;
+
+    public class BaseController : Controller
+    {
+        public BaseController()
+            :this(new PhotoData(new PhotoContext()))
+        {          
+        }
+
+        public BaseController(IPhotoData data)
+        {
+            this.Data = data;
+        }
+
+        protected IPhotoData Data { get; set; }
+    }
+}
