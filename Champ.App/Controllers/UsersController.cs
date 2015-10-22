ce5399fff66cbca672f580b50e09938ca2f41bb8
timvk <tim_vk@abv.bg>
2015-10-22 18:26:09 +0300

    added viewprofile action

---
+++ Champ.App/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Champ.App.Models;
+using Microsoft.AspNet.Identity;
+
+namespace Champ.App.Controllers
+{
+    using System.Web.Mvc;
+
+    public class UsersController : BaseController
+    {
+        [Authorize]
+        public ActionResult ViewProfile()
+        {
+            var loggedUserId = this.User.Identity.GetUserId();
+
+            var userProfile = this.Data.Users.All()
+                .Where(u => u.Id == loggedUserId)
+                .Select(UserProfileViewModel.Create)
+                .FirstOrDefault();
+            
+            return View(userProfile);
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Champ.App/Models/UserProfileViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace Champ.App.Models
+{
+    using System;
+    using System.Linq.Expressions;
+    using Champ.Models;
+
+    public class UserProfileViewModel
+    {
+        public string Username { get; set; }
+
+        public int OwnContest { get; set; }
+
+        public int ParticipatedInContests { get; set; }
+
+        public int WonContests { get; set; }
+
+        public static Expression<Func<User, UserProfileViewModel>> Create
+        {
+            get
+            {
+                return u => new UserProfileViewModel
+                {
+                    Username = u.UserName,
+                    OwnContest = u.CreatedContests.Count,
+                    ParticipatedInContests = u.ParticipatedIn.Count,
+                    WonContests = u.WonContests.Count
+                };
+            }
+        }
+    }
+}
